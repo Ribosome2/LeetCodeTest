753e98259139d4e2e5a30e9a00104e503f09707b
Ribosome2 <583154233@qq.com>
2019-02-03 11:42:24 +0800

    LeetCode test first Commit
    LeetCode test first Commit

---
+++ Assets/Scripts/RotateMatrix.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMatrix {
+    /// <summary>
+    /// 按顺时针遍历矩阵，返回一个列表
+    /// </summary>
+    /// <param name="matirx"></param>
+    /// <returns></returns>
+    public static List<int> Rotate(int[,] matirx)
+    {
+        List<int> resultList = new List<int>();
+        int rowCount = matirx.GetLength(0);
+        int colCount = matirx.GetLength(1);
+        int direction = 0;
+        int rowSteps = rowCount;
+        int colSteps = colCount;
+        int colIndex = -1;
+        int rowIndex = 0;
+        int totalCount = rowCount * colCount;
+        while((rowSteps>0 || colSteps>0) && resultList.Count<totalCount)
+        {
+            int curDir = direction % 4;
+            switch (curDir)
+            {
+                case 0: //To Right
+                    {
+                        for(int i = 0; i < colSteps; i++)
+                        {
+                            colIndex++;
+                            resultList.Add(matirx[rowIndex, colIndex]);
+                        }
+                        rowSteps--;
+                        break;
+                    }
+                case 1: //To Down
+                    {
+                        for (int i = 0; i < rowSteps; i++)
+                        {
+                            rowIndex++;
+                            resultList.Add(matirx[rowIndex, colIndex]);
+                        }
+                        colSteps--;
+                        break;
+                    }
+                case 2: //To Left
+                    {
+                        for (int i = 0; i < colSteps; i++)
+                        {
+                            colIndex--;
+                            resultList.Add(matirx[rowIndex, colIndex]);
+                        }
+                        rowSteps--;
+                        
+                        break;
+                    }
+                case 3: //To Up
+                    {
+                        for (int i = 0; i < rowSteps; i++)
+                        {
+                            rowIndex--;
+                            resultList.Add(matirx[rowIndex, colIndex]);
+                        }
+                        colSteps--;
+                        break;
+                    }
+            }
+            direction++;
+        }
+        return resultList;
+    }
+	
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    private void OnEnable()
+    {
+        var matrix1 = new int[,]
+        {
+            {1,2,3 },
+            {8,9,4 },
+            {7,6,5 },
+        };
+        var matrix2 = new int[,]
+        {
+            {1, 2,  3,  4 },
+            {10,11, 12, 5,},
+            {9, 8,  7 , 6,},
+        };
+
+        var list = RotateMatrix.Rotate(matrix1);
+        PrintList(list);
+        PrintList(RotateMatrix.Rotate(matrix2));
+
+    }
+
+    void PrintList(List<int> list)
+    {
+        var str = "";
+        for(int i=0;i<list.Count;i++)
+        {
+            str += list[i]+",";
+        }
+        Debug.Log(str);
+           
+    }
+
+    
+}
