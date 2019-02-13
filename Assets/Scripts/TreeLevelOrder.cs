1175201f9ea5186fb4fb3d9f9d99b678fb198db6
Ribosome2 <583154233@qq.com>
2019-02-13 11:54:28 +0800

    add

--- Assets/Scripts/TreeLevelOrder.cs
+++ Assets/Scripts/TreeLevelOrder.cs
@@ -10,6 +10,38 @@ namespace Assets.Scripts
         public List<List<int>> LevelOrder(TreeNode root)
         {
             List<List<int>> resultList=new List<List<int>>();
+            if (root != null)
+            {
+                Queue<TreeNode> curQueque=new Queue<TreeNode>();
+                Queue<TreeNode> nextQueque = new Queue<TreeNode>();
+                curQueque.Enqueue(root);
+                while (curQueque.Count>0)
+                {
+                    List<int> list=new List<int>();
+                    while (curQueque.Count>0)
+                    {
+                        var node = curQueque.Dequeue();
+                        list.Add(node.val);
+
+                        if (node.left != null)
+                        {
+                            nextQueque.Enqueue(node.left);
+                        }
+
+                        if (node.right != null)
+                        {
+                            nextQueque.Enqueue(node.right);
+                        }
+                    }
+                    
+                    resultList.Add(list);
+
+                    curQueque = nextQueque;
+                    nextQueque=new Queue<TreeNode>();
+                    
+                }
+
+            }
 
             return resultList;
         }
