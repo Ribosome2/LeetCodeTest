060752992f073bdab0e9f83d66769b75388303b1
Ribosome2 <583154233@qq.com>
2019-02-15 08:16:10 +0800

    Update TestInConsoleApp/TestInConsoleApp/BinaryMath.cs
    add

---
+++ TestInConsoleApp/TestInConsoleApp/BinaryMath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp
+{
+    class BinaryMath
+    {
+        string addBinary(string a, string b)
+        {
+            string strLong;
+            string strShort;
+            if (a.Length > b.Length)
+            {
+                strLong = a;
+                strShort = b;
+            }
+            else
+            {
+                strLong = b;
+                strShort = a;
+            }
+            int temp;
+            LinkedList<int> numLinkedList=new LinkedList<int>();
+            //用十进制立竖式的方法去算
+            for (int i = 0; i < strLong.Length; i++)
+            {
+                char num = strLong[i];
+                char num2 = null;
+                if (i < strShort.Length)
+                {
+                    num2 = strShort[i];
+                }
+
+                if (num2 == num)
+                {
+
+                }
+            }
+
+            
+            return numLinkedList.ToArray();
+        }
+    }
+}
