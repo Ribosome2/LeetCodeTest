ed505c8f933dabe97fe0de94a8f272fe5a429821
Ribosome2 <583154233@qq.com>
2019-02-17 10:26:23 +0800

    旋转图像矩阵90度

--- TestInConsoleApp/TestInConsoleApp/Program.cs
+++ TestInConsoleApp/TestInConsoleApp/Program.cs
@@ -10,8 +10,30 @@ namespace TestInConsoleApp
     {
         static void Main(string[] args)
         {
-            BinaryMath math=new BinaryMath();
-            Console.WriteLine(math.addBinary("11", "1"));
+            RotateMatrix roteMatrix=new RotateMatrix();
+            var m = new int[,]
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9},
+            };
+//            m = new int[,]
+//            {
+//                {1, 2, },
+//                {3, 4,},
+//            };
+
+//            m=new int[,]
+//            {
+//                {5,1,9,11},
+//                {2,4,8,10},
+//                {13,3,6,7},
+//                { 15,14,12,16}
+//            }
+//            ;
+
+            roteMatrix.Rotate(m);
+            Console.Write(m);
             Console.Read();
         }
     }
--- TestInConsoleApp/TestInConsoleApp/RotateMatrix.cs
+++ TestInConsoleApp/TestInConsoleApp/RotateMatrix.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 public class RotateMatrix {
     /// <summary>
@@ -68,5 +69,45 @@ public class RotateMatrix {
         }
         return resultList;
     }
-	
+
+    //给定一个 n × n 的二维矩阵表示一个图像 将图像顺时针旋转 90 度。
+    //你必须在原地旋转图像，这意味着你需要直接修改输入的二维矩阵
+    public void Rotate(int[,] matrix)
+    {
+        //思路:假如是2x2的矩阵{{1，2}{3，4}}，旋转的过程可以表现为:
+        //保存1到临时变量-> 3覆盖1-> 4覆盖3->2覆盖4 临时变量覆盖2
+        int size = matrix.GetLength(0);
+        int temp = 0;
+        int curSqureSize = size;
+        int curRow = 0;
+        int startCol = 0;
+        int right = size - 1;
+        int buttom = size - 1;
+        while (curSqureSize>1)
+        {
+            //每次循环的第一个和最后一个是在 同一次旋转的，所以一行只需要循环n-1
+            int colCurCount = curSqureSize-1; 
+            int offset = 0;
+            while (colCurCount>0)
+            {
+                temp = matrix[curRow, startCol+offset]; //1
+                matrix[curRow, startCol+offset] = matrix[buttom-offset, startCol];//3->1
+                matrix[buttom - offset, startCol] = matrix[buttom, right-offset];//4->3
+                matrix[buttom, right-offset] = matrix[curRow + offset, right]; //2->4
+                matrix[curRow + offset, right] = temp;
+
+                colCurCount -= 1;
+                offset++;
+            }
+
+           
+            curRow++;
+            startCol++;
+            right--;
+            buttom--;
+            //每一次循环正方形半径小2 （上下左右一圈）
+            curSqureSize -= 2;
+        }
+    }
+
 }
