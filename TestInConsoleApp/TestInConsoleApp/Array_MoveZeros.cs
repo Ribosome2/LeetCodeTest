874ac9fe69d8930d549204c3f0462c6e223730ee
Ribosome2 <583154233@qq.com>
2019-02-18 08:21:34 +0800

    add

---
+++ TestInConsoleApp/TestInConsoleApp/Array_MoveZeros.cs
@@ -0,0 +1,47 @@
+﻿namespace TestInConsoleApp
+{
+    public class Array_MoveZeros
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int validIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    if (i > validIndex)
+                    {
+                        nums[validIndex] = nums[i];
+                        nums[i] = 0;
+                    }
+                    validIndex++;
+
+                }
+            }
+
+        }
+
+        public void MoveZeroes2(int[] nums)
+        {
+            int validIndex = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    if (i > validIndex)
+                    {
+                        nums[validIndex] = nums[i];
+                    }
+                    validIndex++;
+
+                }
+            }
+
+            for (int j = validIndex; j < nums.Length; j++)
+            {
+                nums[j] = 0;
+            }
+
+        }
+    }
+}
---
+++ TestInConsoleApp/TestInConsoleApp/LinkList_RemoveNthFromEnd.cs
@@ -0,0 +1,25 @@
+﻿namespace TestInConsoleApp
+{
+    public class LinkList_RemoveNthFromEnd
+    {
+//        给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            ListNode fast = head, slow = head;
+            while (n > 0)
+            {
+                fast = fast.next;
+                n--;
+            }
+            if (fast == null)
+                return slow.next;
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+            return head;
+        }
+    }
+}
