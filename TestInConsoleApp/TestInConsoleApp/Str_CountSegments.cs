5768ad57ae5d20386e36e59367799f06601dbf92
Ribosome2 <583154233@.qq.com>
2019-02-27 15:06:28 +0800

    检测大小写

---
+++ TestInConsoleApp/TestInConsoleApp/Str_CountSegments.cs
@@ -0,0 +1,61 @@
+﻿namespace TestInConsoleApp
+{
+    public class Str_CountSegments
+    {
+
+        //统计字符串中的单词个数，这里的单词指的是连续的不是空格的字符。
+        //请注意，你可以假定字符串里不包括任何不可打印的字符。
+        public int CountSegments(string s)
+        {
+            int count = 0;
+            int notEmptyCount = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    if (notEmptyCount > 0)
+                    {
+                        count++;
+                    }
+
+                    notEmptyCount = 0;
+                }
+                else
+                {
+                    notEmptyCount++;
+                }
+            }
+            if (notEmptyCount > 0)
+            {
+                count++;
+            }
+            return count;
+        }
+        public int CountSegments1(string s)
+        {
+            int count = 0;
+            bool notEmpty = false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    if (notEmpty)
+                    {
+                        count++;
+                    }
+
+                    notEmpty = false;
+                }
+                else
+                {
+                    notEmpty=true;
+                }
+            }
+            if (notEmpty )
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
---
+++ TestInConsoleApp/TestInConsoleApp/Str_DetectCapitalUse.cs
@@ -0,0 +1,45 @@
+﻿namespace TestInConsoleApp
+{
+    public class Str_DetectCapitalUse
+    {
+        //给定一个单词，你需要判断单词的大写使用是否正确。
+
+        //我们定义，在以下情况时，单词的大写用法是正确的：
+
+        //全部字母都是大写，比如"USA"。
+        //单词中所有字母都不是大写，比如"leetcode"。
+        //如果单词不只含有一个字母，只有首字母大写， 比如 "Google"。
+        //否则，我们定义这个单词没有正确使用大写字母。
+        public bool DetectCapitalUse(string word)
+        {
+            int capFactor = 0;
+            int lowerSum = 0;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (IsCapital(word[i]))
+                {
+                    if (i != 0) //只有首字母可以大写
+                    {
+                        capFactor = 1;
+                    }
+                }
+                else
+                {
+                    lowerSum++;
+                }
+
+                if (capFactor * lowerSum > 0) //大小写都有了并且不是首字母的时候会大于0
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //是否大写
+        bool IsCapital(char c)
+        {
+            return c > 64 && c < 91;
+        }
+    }
+}
