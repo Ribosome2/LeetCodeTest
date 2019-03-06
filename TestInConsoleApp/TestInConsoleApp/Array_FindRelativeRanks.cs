77e1122e391dead18cdcead07975c9ccd9e0a615
Ribosome2 <583154233@.qq.com>
2019-03-06 12:39:43 +0800

    add

---
+++ TestInConsoleApp/TestInConsoleApp/Array_FindRelativeRanks.cs
@@ -0,0 +1,24 @@
+﻿namespace TestInConsoleApp
+{
+    public class Array_FindRelativeRanks
+    {
+        //给出 N 名运动员的成绩，找出他们的相对名次并授予前三名对应的奖牌。
+        //前三名运动员将会被分别授予 “金牌”，“银牌” 和“ 铜牌”（"Gold Medal", "Silver Medal", "Bronze Medal"）。
+        //(注：分数越高的选手，排名越靠前。)
+        //示例 1:
+        private readonly string[] TOP_STR = new string[] {"Gold Medal", "Silver Medal", "Bronze Medal"};
+
+
+        public string[] FindRelativeRanks(int[] nums)
+        {
+            string[] result=new string[nums.Length];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                
+            }
+            return result;
+        }
+
+    }
+}
---
+++ TestInConsoleApp/TestInConsoleApp/LinkList_MiddleNode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp
+{
+    class LinkList_MiddleNode
+    {
+        //给定一个带有头结点 head 的非空单链表，返回链表的中间结点。
+        //如果有两个中间结点，则返回第二个中间结点。
+        public ListNode MiddleNode(ListNode head)
+        {
+            int nodeCount = 0;
+            ListNode curNode = head;
+            while (curNode!=null)
+            {
+                nodeCount++;
+                curNode = curNode.next;
+            }
+
+            int middleIndex = 0;
+            middleIndex = nodeCount / 2;
+
+            curNode = head;
+            while (middleIndex>0)
+            {
+                middleIndex--;
+                curNode = curNode.next;
+            }
+
+            return curNode;
+        }
+
+        public ListNode MiddleNode1(ListNode head)
+        {
+            //两个指针，快的比慢的走快一半，当快的走到头的时候，慢的就在中间
+            var slow = head;
+            var fast = head;
+            while (fast!=null && fast.next!=null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+}
