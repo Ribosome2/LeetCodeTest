f4b58a469ca5d6d4cbe9c41f286c6ee2159bd457
Ribosome2 <583154233@qq.com>
2019-03-13 08:04:40 +0800

    add

---
+++ TestInConsoleApp/TestInConsoleApp/Str_CommonChars.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class Str_CommonChars
+    {
+
+//        给定仅有小写字母组成的字符串数组 A，返回列表中的每个字符串中都显示的全部字符（包括重复字符）组成的列表。
+//        例如，如果一个字符在每个字符串中出现 3 次，但不是 4 次，则需要在最终答案中包含该字符 3 次。
+//
+//        你可以按任意顺序返回答案。
+        public IList<string> CommonChars(string[] A)
+        {
+            Dictionary<char ,int> countDict=new Dictionary<char, int>();
+
+            List<string> res=new  List<string>();
+            return res;
+        }
+    }
+}
