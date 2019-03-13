517e7881f6e37453ba67386fa6acb6eb8836885f
Ribosome2 <583154233@.qq.com>
2019-03-13 12:00:00 +0800

    ToLowerCase

--- TestInConsoleApp/TestInConsoleApp/Str_ToLowerCase.cs
+++ TestInConsoleApp/TestInConsoleApp/Str_ToLowerCase.cs
@@ -7,21 +7,47 @@ namespace TestInConsoleApp
         //实现函数 ToLowerCase()，该函数接收一个字符串参数 str，并将该字符串中的大写字母转换成小写字母，之后返回新的字符串。
         public string ToLowerCase(string str)
         {
-            StringBuilder sb=new StringBuilder();
+            StringBuilder sb = null;
             for (int i = 0; i < str.Length; i++)
             {
-                sb.Append(GetLower(str[i]));
+                if (IsCapital(str[i]))
+                {
+                    if (sb == null)
+                    {
+                        sb=new StringBuilder();
+                    }
+                    for (int index = sb.Length; index < i; index++)
+                    {
+                        sb.Append(str[index]);
+                    }
+                    sb.Append((char)(str[i]+32));
+                }
             }
 
+
+            if (sb==null || sb.Length == 0)
+            {
+                return str;
+            }
+            else
+            {
+                for (int index = sb.Length; index < str.Length; index++)
+                {
+                    sb.Append(str[index]);
+                }
+            }
             return sb.ToString();
 
         }
-
+        bool IsCapital(char c)
+        {
+            return c > 64 && c < 91;
+        }
         char GetLower(char c)
         {
             if (IsCapital(c))
             {
-                return (char)(c-32);
+                return (char)(c+32);
             }
             else
             {
@@ -29,9 +55,6 @@ namespace TestInConsoleApp
             }
         }
 
-        bool IsCapital(char c)
-        {
-            return c > 64 && c < 91;
-        }
+        
     }
 }
