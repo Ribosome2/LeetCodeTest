41e09c4e3cbc5486978e2054dda2265ed770286a
Ribosome2 <583154233@qq.com>
2019-03-14 08:09:03 +0800

    addd

---
+++ TestInConsoleApp/TestInConsoleApp/Str_ReverseOnlyLetters.cs
@@ -0,0 +1,12 @@
+﻿namespace TestInConsoleApp
+{
+    public class Str_ReverseOnlyLetters
+    {
+        //给定一个字符串 S，返回 “反转后的” 字符串，其中不是字母的字符都保留在原地，而所有字母的位置发生反转。
+        public string ReverseOnlyLetters(string S)
+        {
+            char[] arr=new  char[S.Length];
+            return arr.ToString();
+        }
+    }
+}
