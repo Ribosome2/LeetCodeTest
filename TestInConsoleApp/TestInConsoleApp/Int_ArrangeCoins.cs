1826776b966e0e002f7bec33f2560b5c83177178
Ribosome2 <583154233@.qq.com>
2019-03-22 10:29:35 +0800

    公式求解

--- TestInConsoleApp/TestInConsoleApp/Int_ArrangeCoins.cs
+++ TestInConsoleApp/TestInConsoleApp/Int_ArrangeCoins.cs
@@ -1,4 +1,6 @@
-﻿namespace TestInConsoleApp
+﻿using System;
+
+namespace TestInConsoleApp
 {
     public class Int_ArrangeCoins
     {
@@ -14,6 +16,13 @@
                 level++;
             }
             return level-1;
+        }
+
+        public int ArrangeCoins1(int n)
+        {
+            //公式求解：根据等差数列求和公式SumN=N*a1+N*(N-1)*d/2 (这里d为1,a1=1）
+            //-> SumN = (N*N+N)/2
+            return (int)(-1 + Math.Sqrt(1 + 8 * (long)n)) / 2;
         }
     }
 }
