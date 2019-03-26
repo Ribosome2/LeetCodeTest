c636c94f9f16bca1791d1aa52826cc5ae62e7513
KyleLin <583154233@qq.com>
2019-03-26 08:01:21 +0800

    二叉树取出从低层向上的列表顺序

---
+++ TestInConsoleApp/TestInConsoleApp/Tree_LevelOrderBottom.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class Tree_LevelOrderBottom
+    {
+        //给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+        public List<List<int>> LevelOrderBottom(TreeNode root)
+        {
+            List<List<int>> result=new List<List<int>>();
+            if (root != null)
+            {
+                Queue<TreeNode> queque = new Queue<TreeNode>();
+                Stack<List<int> > resultStack=new Stack<List<int>>();
+                queque.Enqueue(root);
+                while (queque.Count>0)
+                {
+                    int count = queque.Count;
+                    List<int> list = new List<int>();
+                    for (int i = 0; i < count; i++)
+                    {
+                        var node = queque.Dequeue();
+                       
+                        list.Add(node.val);
+                        if (node.left != null)
+                        {
+                            queque.Enqueue(node.left);
+                        }
+
+                        if (node.right != null)
+                        {
+                            queque.Enqueue(node.right);
+                        }
+                    }
+                    resultStack.Push(list);
+                }
+
+                while (resultStack.Count>0)
+                {
+                    result.Add(resultStack.Pop());
+                }
+            }
+
+          
+
+            return result;
+        }
+    }
+}
