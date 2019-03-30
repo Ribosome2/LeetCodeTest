d8480b1a1365ecbff459d2abe2f677011a09fe9d
Ribosome2 <583154233@.qq.com>
2019-03-30 17:18:45 +0800

    数字的二进制取反

--- TestInConsoleApp/TestInConsoleApp/Program.cs
+++ TestInConsoleApp/TestInConsoleApp/Program.cs
@@ -11,20 +11,15 @@ namespace TestInConsoleApp
     {
         static void Main(string[] args)
         {
-            var test = new String_ValidPalindrome();;
+            var test = new Int_FindComplement();;
             //var arr = new int[,]{{44, 35, 39},{15, 44, 35},{17, 15, 44},{80, 17, 15},{43, 80, 0},{77, 43, 80}};
             var arr = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
-            string str = "rrr";
-            TestStr(ref str);
-            Console.WriteLine(str);
+            Console.WriteLine(test.FindComplement(0));
             Console.Read();
         }
 
 
-        static void TestStr(ref string ttt)
-        {
-            ttt += "ddd";
-        }
+       
     }
 }
