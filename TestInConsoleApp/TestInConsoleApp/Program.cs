c102d1d3e4a61779dc81c02c580b3a95ccd6d8bc
Ribosome2 <583154233@.qq.com>
2019-03-30 18:07:51 +0800

    斐波那契数 求N项

--- TestInConsoleApp/TestInConsoleApp/Program.cs
+++ TestInConsoleApp/TestInConsoleApp/Program.cs
@@ -11,11 +11,11 @@ namespace TestInConsoleApp
     {
         static void Main(string[] args)
         {
-            var test = new Int_FindComplement();;
+            var test = new Int_Fib();;
             //var arr = new int[,]{{44, 35, 39},{15, 44, 35},{17, 15, 44},{80, 17, 15},{43, 80, 0},{77, 43, 80}};
             var arr = new int[] {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
-            Console.WriteLine(test.FindComplement(0));
+            Console.WriteLine(test.Fib1(6));
             Console.Read();
         }
 
