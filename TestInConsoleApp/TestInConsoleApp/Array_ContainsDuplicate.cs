2f0b486af7791c6b3e029d71e0a88f3416d2b738
KyleLin <583154233@qq.com>
2019-04-01 07:48:08 +0800

    add

---
+++ TestInConsoleApp/TestInConsoleApp/Array_ContainsDuplicate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class Array_ContainsDuplicate
+    {
+        //给定一个整数数组，判断是否存在重复元素。
+        //如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int,bool> dict=new Dictionary<int, bool>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+                if (dict.ContainsKey(n))
+                {
+                    return true;
+                }
+                else
+                {
+                    dict[n] = true;
+                }
+            }
+            return false;
+        }
+
+        //给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums[i] = nums[j]，并且 i 和 j 的差的绝对值最大为 k。
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            return false;
+        }
+    }
+}
