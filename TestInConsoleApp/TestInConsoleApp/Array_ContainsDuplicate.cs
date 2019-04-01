e9857e531f61f4fcadf97ad8538060f5d03dd6eb
KyleLin <583154233@qq.com>
2019-04-01 08:09:25 +0800

    存在重复元素 II

--- TestInConsoleApp/TestInConsoleApp/Array_ContainsDuplicate.cs
+++ TestInConsoleApp/TestInConsoleApp/Array_ContainsDuplicate.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace TestInConsoleApp
 {
@@ -27,6 +28,42 @@ namespace TestInConsoleApp
         //给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums[i] = nums[j]，并且 i 和 j 的差的绝对值最大为 k。
         public bool ContainsNearbyDuplicate(int[] nums, int k)
         {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int maxIndex = Math.Max(nums.Length, i + k + 1);
+                for (int j = i + 1; j < maxIndex; j++)
+                {
+                    if (nums[i] == nums[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool ContainsNearbyDuplicate1(int[] nums, int k)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var n = nums[i];
+                if (dict.ContainsKey(n))
+                {
+                    if (i - dict[n] <= k)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        dict[n] = i;
+                    }
+                }
+                else
+                {
+                    dict[n] = i;
+                }
+            }
             return false;
         }
     }
