df85d2b2d32986584f6644a9656aaa7e25f01385
Ribosome2 <583154233@.qq.com>
2019-04-09 10:51:18 +0800

    add

---
+++ TestInConsoleApp/TestInConsoleApp/MyHashSet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class MyHashSet
+    {
+        /** Initialize your data structure here. */
+        public MyHashSet()
+        {
+
+        }
+        public void Add(int key)
+        {
+
+        }
+
+        public void Remove(int key)
+        {
+
+        }
+
+        /** Returns true if this set contains the specified element */
+        public bool Contains(int key)
+        {
+            return false;
+        }
+    }
+}
