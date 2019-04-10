72a09ab68be1b1de591d4b94a0d0a7f6b8568151
KyleLin <583154233@qq.com>
2019-04-10 21:31:32 +0800

    N叉树的最大深度

---
+++ TestInConsoleApp/TestInConsoleApp/Tree_MaxDepth.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class Tree_MaxDepth
+    {
+        // Definition for a Node.
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node() { }
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+
+            public int MaxDepth(Node root)
+            {
+                if (root == null)
+                {
+                    return 0;
+                }
+
+                if (root.children.Count == 0)
+                {
+                    return 1;
+                }
+
+                int maxDepth = 1;
+                for (int i = 0; i < root.children.Count; i++)
+                {
+                    int depth= MaxDepth(root.children[i]);
+                    if (depth > maxDepth)
+                    {
+                        maxDepth = depth;
+                    }
+                }
+
+                return maxDepth+1;
+            }
+        }
+    }
+}
