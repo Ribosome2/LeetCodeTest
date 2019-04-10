1020138f97da4154581f8fb7448e02096f700b07
Ribosome2 <583154233@.qq.com>
2019-04-10 17:05:53 +0800

    pieceOf case

---
+++ TestInConsoleApp/TestInConsoleApp/JudgeCircle.cs
@@ -0,0 +1,39 @@
+namespace TestInConsoleApp
+{
+    public class Str_JudgeCircle
+    {
+        public bool JudgeCircle(string moves)
+        {
+            int x=0, y=0;
+            for (int i = 0; i < moves.Length; i++)
+            {
+                switch (moves[i])
+                {
+                    case 'U':
+                    {
+                        y++;
+                        break;
+                    }
+                    case 'D':
+                    {
+                        y--;
+                        break;
+                    }
+                    case 'L':
+                    {
+                        x--;
+                        break;
+                    }
+                    case 'R':
+                    {
+                        x++;
+                        break;
+                    }
+                    
+                }
+            }
+            return (x == 0 && y == 0);
+
+        }
+    }
+}
