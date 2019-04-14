481acbd786486442611acf9e1f44aef533395210
KyleLin <583154233@qq.com>
2019-04-14 08:25:51 +0800

    迭代法实现 N叉树的前序遍历

--- TestInConsoleApp/TestInConsoleApp/Tree_Preorder.cs
+++ TestInConsoleApp/TestInConsoleApp/Tree_Preorder.cs
@@ -22,6 +22,35 @@ namespace TestInConsoleApp
             }
         }
 
+
+        public IList<int> Preorder1(Node root)
+        {
+            List<int> list = new List<int>();
+            
+            if (root != null)
+            {
+                //建立一个栈，入栈的时候从右边开始入栈，就可以保证是从左边开始遍历
+                Stack<Node> stack = new Stack<Node>();
+                stack.Push(root);
+                while (stack.Count>0)
+                {
+                    int count = stack.Count;
+                    while (count>0)
+                    {
+                        count--;
+                        var node = stack.Pop();
+                        list.Add(node.val);
+                        for (int i = node.children.Count - 1; i >= 0; i--)
+                        {
+                            stack.Push(node.children[i]);
+                        }
+                    }
+                }
+
+            }
+            return list;
+        }
+
         //给定一个 N 叉树，返回其节点值的前序遍历。
         public IList<int> Preorder(Node root)
         {
