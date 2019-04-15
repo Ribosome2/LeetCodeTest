618c02c4b4e05d99a520e1a9fe16548d9a5ea060
Ribosome2 <583154233@.qq.com>
2019-04-15 15:02:34 +0800

    N叉树的层序遍历

---
+++ TestInConsoleApp/TestInConsoleApp/NTree_LevelOrder.cs
@@ -0,0 +1,54 @@
+using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class NTree_LevelOrder
+    {
+
+
+// Definition for a Node.
+        public class Node
+        {
+            public int val;
+            public IList<Node> children;
+
+            public Node()
+            {
+            }
+
+            public Node(int _val, IList<Node> _children)
+            {
+                val = _val;
+                children = _children;
+            }
+        }
+
+//        给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            IList<IList<int>> re = new List<IList<int>>();
+            if (root != null)
+            {
+                Queue<Node> queue=new Queue<Node>();
+                queue.Enqueue(root);
+                while (queue.Count>0)
+                {
+                    int count = queue.Count;
+                    List<int> list=new List<int>();
+                    while (count>0)
+                    {
+                        var node = queue.Dequeue();
+                        list.Add(node.val);
+                        for (int i = 0; i < node.children.Count; i++)
+                        {
+                            
+                        }
+                        count--;
+                    }
+                    re.Add(list);
+                }
+            }
+            return re;
+        }
+    }
+}
--- TestInConsoleApp/TestInConsoleApp/QuadTree_Intersect.cs
+++ TestInConsoleApp/TestInConsoleApp/QuadTree_Intersect.cs
@@ -2,33 +2,34 @@
 
 namespace TestInConsoleApp
 {
-    // Definition for a QuadTree node.
-    public class Node
-    {
-        public bool val;
-        public bool isLeaf;
-        public Node topLeft;
-        public Node topRight;
-        public Node bottomLeft;
-        public Node bottomRight;
-
-        public Node()
-        {
-        }
-
-        public Node(bool _val, bool _isLeaf, Node _topLeft, Node _topRight, Node _bottomLeft, Node _bottomRight)
-        {
-            val = _val;
-            isLeaf = _isLeaf;
-            topLeft = _topLeft;
-            topRight = _topRight;
-            bottomLeft = _bottomLeft;
-            bottomRight = _bottomRight;
-        }
-    }
+ 
 
     public class QuadTree_Intersect
     {
+        // Definition for a QuadTree node.
+        public class Node
+        {
+            public bool val;
+            public bool isLeaf;
+            public Node topLeft;
+            public Node topRight;
+            public Node bottomLeft;
+            public Node bottomRight;
+
+            public Node()
+            {
+            }
+
+            public Node(bool _val, bool _isLeaf, Node _topLeft, Node _topRight, Node _bottomLeft, Node _bottomRight)
+            {
+                val = _val;
+                isLeaf = _isLeaf;
+                topLeft = _topLeft;
+                topRight = _topRight;
+                bottomLeft = _bottomLeft;
+                bottomRight = _bottomRight;
+            }
+        }
 
         public Node Intersect(Node quadTree1, Node quadTree2)
         {
