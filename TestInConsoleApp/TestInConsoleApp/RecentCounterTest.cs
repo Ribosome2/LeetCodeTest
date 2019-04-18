67fc48dcbabb47f32d3db3017c485edc3fc6ed58
KyleLin <583154233@qq.com>
2019-04-18 08:05:40 +0800

    Number of Recent Calls

---
+++ TestInConsoleApp/TestInConsoleApp/RecentCounterTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class RecentCounterTest
+    {
+        public class RecentCounter
+        {
+            Queue<int> pinQueue=new Queue<int>();
+            public RecentCounter()
+            {
+
+            }
+
+            public int Ping(int t)
+            {
+                while (true)
+                {
+                    if (pinQueue.Count == 0)
+                    {
+                        break;
+                    }
+                    if (t - pinQueue.Peek() <= 3000)
+                    {
+                        break;
+                    }
+
+                    pinQueue.Dequeue();
+                }
+                
+                pinQueue.Enqueue(t);
+                return pinQueue.Count;
+            }
+        }
+    }
+}
