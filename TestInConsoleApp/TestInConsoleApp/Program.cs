146dd014d225c6761fa84cecdc5bab6ad0c06d6c
Ribosome2 <583154233@qq.com>
2019-04-20 19:38:32 +0800

    反转字符串 II

--- TestInConsoleApp/TestInConsoleApp/Program.cs
+++ TestInConsoleApp/TestInConsoleApp/Program.cs
@@ -11,6 +11,11 @@ namespace TestInConsoleApp
     {
         static void Main(string[] args)
         {
+            string str="abc";
+            char[] arr=new char[3];
+//            new Str_ReverseChaArray().ReverseCharArr(ref str, arr, 0, 2);
+//            Console.WriteLine(new string(arr));
+            Console.WriteLine(new Str_ReverseChaArray().ReverseStr("abcdefg",2));
             Console.Read();
         }
 
--- TestInConsoleApp/TestInConsoleApp/Str_ReverseChaArray.cs
+++ TestInConsoleApp/TestInConsoleApp/Str_ReverseChaArray.cs
@@ -1,4 +1,6 @@
-﻿namespace TestInConsoleApp
+﻿using System;
+
+namespace TestInConsoleApp
 {
     public class Str_ReverseChaArray
     {
@@ -14,6 +16,43 @@
                 left++;
                 right--;
             }
+        }
+        // 给定一个字符串和一个整数 k，你需要对从字符串开头算起的每个 2k 个字符的前k个字符进行反转。如果剩余少于 k 个字符，
+        // 则将剩余的所有全部反转。如果有小于 2k 但大于或等于 k 个字符，则反转前 k 个字符，并将剩余的字符保持原样。
+        public string ReverseStr(string s, int k)
+        {
+            var chaArr = new char[s.Length];
+            int index = 0;
+            int reverseCount = k;
+            //每累计K个进行一次翻转
+            while (index<s.Length)
+            {
+                if (reverseCount < k)
+                {
+                    reverseCount++;
+                    chaArr[index] = s[index];
+                    index++;
+                }
+                else
+                {
+                    reverseCount = 0;
+                    ReverseCharArr(ref s, chaArr, index, Math.Min(index + k-1,s.Length-1));
+                    index += k;
+                }
+            }
+            return new string(chaArr);
+        }
+
+        public void ReverseCharArr(ref string str,char[] chArr, int left, int right)
+        {
+            while (left < right)
+            {
+                chArr[left] = str[right];
+                chArr[right] = str[left];
+                left++;
+                right--;
+            }
+            chArr[left] = str[left];
         }
     }
 }
