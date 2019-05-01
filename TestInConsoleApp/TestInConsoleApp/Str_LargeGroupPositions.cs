8e9a18412c8e6bf624ef16b8fce3d8763b90bb0e
Ribosome2 <583154233@qq.com>
2019-05-01 17:13:17 +0800

    较大分组的位置

---
+++ TestInConsoleApp/TestInConsoleApp/Str_LargeGroupPositions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class Str_LargeGroupPositions
+    {
+        public IList<IList<int>> LargeGroupPositions(string S)
+        {
+            List<IList<int>> resList=new List<IList<int>>();
+            int left = 0;
+            char temp = S[0];
+            for (int i = 1; i < S.Length; i++)
+            {
+                char ch = S[i];
+                if (ch != temp)
+                {
+                    if (i - left > 3)
+                    {
+                        resList.Add(new List<int>(){left,i-1});
+                    }
+
+                    left = i;
+                    temp = ch;
+                }
+            }
+            if ((S.Length -1)- left >= 3)
+            {
+                resList.Add(new List<int>() { left, S.Length - 1 });
+            }
+            return resList;
+        }
+    }
+}
