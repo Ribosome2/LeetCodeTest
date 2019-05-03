235e1363d4639d9670b2f107ee18e0df760b43df
Ribosome2 <583154233@qq.com>
2019-05-03 12:51:01 +0800

    add

---
+++ TestInConsoleApp/TestInConsoleApp/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TestInConsoleApp
+{
+    public class Solution
+    {
+        private ListNode mHead;
+
+        private int listCount = 0;
+        /** @param head The linked list's head.
+            Note that the head is guaranteed to be not null, so it contains at least one node. */
+        public Solution(ListNode head)
+        {
+            mHead = head;
+            var node = head;
+            while (node!=null)
+            {
+                listCount++;
+                node = node.next;
+            }
+        }
+
+        /** Returns a random node's value. */
+        public int GetRandom()
+        {
+            //todo this is not correct ,yet
+            int rndIndex = new System.Random().Next(0, listCount);
+            var node = mHead;
+            while (rndIndex>0)
+            {
+                rndIndex--;
+                node = node.next;
+            }
+
+            return node.val;
+        }
+    }
+}
