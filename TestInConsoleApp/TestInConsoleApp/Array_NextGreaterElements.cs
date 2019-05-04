c85d9a577f953257919653592d2a049474eae9dd
Ribosome2 <583154233@qq.com>
2019-05-04 09:43:17 +0800

    NextGreaterElements

---
+++ TestInConsoleApp/TestInConsoleApp/Array_NextGreaterElements.cs
@@ -0,0 +1,42 @@
+﻿namespace TestInConsoleApp
+{
+    public class Array_NextGreaterElements
+    {
+        public int[] NextGreaterElements(int[] nums)
+        {
+            int[] re=new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+
+                re[i] = FindGreaterIndex(nums, i);
+            }
+            return re;
+        }
+
+        int FindGreaterIndex(int[] nums,int curIndex)
+        {
+            int index = curIndex;
+            while (true)
+            {
+                index++;
+                if (index == nums.Length)
+                {
+                    index = 0;
+                }
+
+                if (index == curIndex)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (nums[index] > nums[curIndex])
+                    {
+                        return nums[index];
+                    }
+                }
+            }
+        }
+    }
+}
