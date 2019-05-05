77a31687cbd1027c0da0f6585d7bfc28f77267b4
KyleLin <583154233@qq.com>
2019-05-05 08:22:29 +0800

    Create LinkList_DeleteDuplicates.cs

---
+++ TestInConsoleApp/TestInConsoleApp/LinkList_DeleteDuplicates.cs
@@ -0,0 +1,39 @@
+﻿namespace TestInConsoleApp
+{
+    public class LinkList_DeleteDuplicates
+    {
+        //给定一个排序链表，删除所有含有重复数字的节点，只保留原始链表中 没有重复出现 的数字
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var preNode =FindUniqueNode(head);
+            var curNode = preNode;
+            while (curNode!=null)
+            {
+                curNode.next = FindUniqueNode(curNode);
+                curNode = curNode.next;
+            }
+
+            return preNode;
+        }
+
+        ListNode FindUniqueNode(ListNode node)
+        {
+            if (node != null)
+            {
+                while (node!=null)
+                {
+                    if (node.next == null || node.next.val != node.val)
+                    {
+                        return node;
+                    }
+                    else
+                    {
+                        node = node.next;
+                    }
+                }
+                
+            }
+            return null;
+        }
+    }
+}
