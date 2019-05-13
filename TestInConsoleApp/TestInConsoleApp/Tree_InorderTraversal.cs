51db1e07591d6500737c718d3fe9c71867e99ab6
Ribosome2 <583154233@qq.com>
2019-05-13 16:22:20 +0800

    Create Tree_InorderTraversal.cs

---
+++ TestInConsoleApp/TestInConsoleApp/Tree_InorderTraversal.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace TestInConsoleApp
+{
+    public class Tree_InorderTraversal
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            List<int> reList=new List<int>();
+            RecursiveTraversal(root,reList);
+            return reList;
+        }
+
+        void RecursiveTraversal(TreeNode node,List<int> list)
+        {
+            if (node != null)
+            {
+                RecursiveTraversal(node.left,list);
+                list.Add(node.val);
+                RecursiveTraversal(node.right,list);
+            }
+        }
+
+        public IList<int> InorderTraversal_NoRecursive(TreeNode root)
+        {
+            List<int> reList = new List<int>();
+           
+            Stack<TreeNode> stack=new Stack<TreeNode>();
+            TreeNode node = root;
+            while (node!=null || stack.Count!=0)
+            {
+                if (node != null)
+                {
+                    stack.Push(node);
+                    node = node.left;
+                }
+                else
+                {
+                    var tempNode = stack.Pop();
+                    reList.Add(tempNode.val);
+                    node = tempNode.right;
+                }
+            }
+
+            return reList;
+        }
+    }
+}
