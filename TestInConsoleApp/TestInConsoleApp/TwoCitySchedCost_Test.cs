31603ba71ed059f7972dbab7c0bf70465ae6f2b7
Ribosome2 <583154233@qq.com>
2019-05-18 11:08:30 +0800

    Update TwoCitySchedCost_Test.cs

--- TestInConsoleApp/TestInConsoleApp/TwoCitySchedCost_Test.cs
+++ TestInConsoleApp/TestInConsoleApp/TwoCitySchedCost_Test.cs
@@ -4,6 +4,8 @@ namespace TestInConsoleApp
 {
     public class TwoCitySchedCost_Test
     {
+//        公司计划面试 2N 人。第 i 人飞往 A 市的费用为 costs[i][0]，飞往 B 市的费用为 costs[i][1]。
+//        返回将每个人都飞到某座城市的最低费用，要求每个城市都有 N 人抵达。
         public int TwoCitySchedCost(int[][] costs)
         {
             int sum = 0;
@@ -23,7 +25,7 @@ namespace TestInConsoleApp
                 }
             }
 
-            var halfNum = costs.Length / 2;
+            var halfNum = costs.Length >> 1;
             for (int i = 0; i < halfNum; i++)
             {
                 sum += costs[i][1];
@@ -32,7 +34,5 @@ namespace TestInConsoleApp
 
             return sum;
         }
-
-      
     }
 }
