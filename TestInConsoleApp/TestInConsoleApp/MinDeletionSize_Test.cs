ce50b85429950838cf996117468f8ee26ffebe42
Ribosome2 <583154233@qq.com>
2019-05-18 09:44:59 +0800

    Create MinDeletionSize_Test.cs

---
+++ TestInConsoleApp/TestInConsoleApp/MinDeletionSize_Test.cs
@@ -0,0 +1,29 @@
+﻿namespace TestInConsoleApp
+{
+    public class MinDeletionSize_Test
+    {
+        public int MinDeletionSize(string[] A)
+        {
+            int deleteCount = 0;
+
+            if (A.Length > 1)
+            {
+                int count = A[0].Length;
+
+                for (int i = 0; i < count; i++)
+                {
+                    for (int j = 1; j < A.Length; j++)
+                    {
+                        if (A[j][i] < A[j - 1][i])
+                        {
+                            deleteCount++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return deleteCount;
+        }
+    }
+}
