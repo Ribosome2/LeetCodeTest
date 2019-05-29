05807b689ee8d7c5c226e77c234d03372d46e954
Ribosome2 <583154233@qq.com>
2019-05-29 17:38:56 +0800

    Update LastStoneWeight_Test.cs

--- TestInConsoleApp/TestInConsoleApp/LastStoneWeight_Test.cs
+++ TestInConsoleApp/TestInConsoleApp/LastStoneWeight_Test.cs
@@ -64,7 +64,10 @@ namespace TestInConsoleApp
 
             public void Push(T v)
             {
-                if (Count >= heap.Length) Array.Resize(ref heap, Count * 2);
+                if (Count >= heap.Length)
+                {
+                    Array.Resize(ref heap, Count * 2);
+                }
                 heap[Count] = v;
                 SiftUp(Count++);
             }
@@ -73,20 +76,29 @@ namespace TestInConsoleApp
             {
                 var v = Top();
                 heap[0] = heap[--Count];
-                if (Count > 0) SiftDown(0);
+                if (Count > 0)
+                {
+                    SiftDown(0);
+                }
                 return v;
             }
 
             public T Top()
             {
-                if (Count > 0) return heap[0];
+                if (Count > 0)
+                {
+                    return heap[0];
+                }
                 throw new InvalidOperationException("优先队列为空");
             }
 
             void SiftUp(int n)
             {
                 var v = heap[n];
-                for (var n2 = n / 2; n > 0 && comparer.Compare(v, heap[n2]) > 0; n = n2, n2 /= 2) heap[n] = heap[n2];
+                for (var n2 = n / 2; n > 0 && comparer.Compare(v, heap[n2]) > 0; n = n2, n2 /= 2)
+                {
+                    heap[n] = heap[n2];
+                }
                 heap[n] = v;
             }
 
@@ -95,8 +107,15 @@ namespace TestInConsoleApp
                 var v = heap[n];
                 for (var n2 = n * 2; n2 < Count; n = n2, n2 *= 2)
                 {
-                    if (n2 + 1 < Count && comparer.Compare(heap[n2 + 1], heap[n2]) > 0) n2++;
-                    if (comparer.Compare(v, heap[n2]) >= 0) break;
+                    if (n2 + 1 < Count && comparer.Compare(heap[n2 + 1], heap[n2]) > 0)
+                    {
+                        n2++;
+                    }
+
+                    if (comparer.Compare(v, heap[n2]) >= 0)
+                    {
+                        break;
+                    }
                     heap[n] = heap[n2];
                 }
                 heap[n] = v;
