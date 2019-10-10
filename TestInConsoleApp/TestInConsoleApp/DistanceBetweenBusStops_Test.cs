66f6c21280ec3e424679d362911e58e4e5e46f64
qinshilin <qinshilin@aobi.com>
2019-10-10 19:57:33 +0800

    公交站的距离更优的算法

--- TestInConsoleApp/TestInConsoleApp/DistanceBetweenBusStops_Test.cs
+++ TestInConsoleApp/TestInConsoleApp/DistanceBetweenBusStops_Test.cs
@@ -14,12 +14,13 @@ namespace TestInConsoleApp
             {
                 return 0;
             }
+
             //两边同时走，在没有到达目标之前一直往当前消耗最小的路走
             int goUpDist = 0;
             int goDownDist = 0;
             int goUpIndex = start;
             int goDownIndex = start;
-           
+
             while (true)
             {
                 if (goUpDist > goDownDist)
@@ -28,7 +29,7 @@ namespace TestInConsoleApp
                     goDownIndex = GoDown(distance, goDownIndex, ref goDownDist);
                     if (goDownIndex == destination)
                     {
-                        while (goDownDist>goUpDist && goUpIndex!=destination)
+                        while (goDownDist > goUpDist && goUpIndex != destination)
                         {
                             goUpIndex = GoUp(distance, goUpIndex, ref goUpDist);
                         }
@@ -73,12 +74,13 @@ namespace TestInConsoleApp
 
             if (goUpIndex == 0)
             {
-                goUpDist += distance[distance.Length-1];
+                goUpDist += distance[distance.Length - 1];
             }
             else
             {
-                goUpDist += distance[goUpIndex-1];
+                goUpDist += distance[goUpIndex - 1];
             }
+
             return goUpIndex;
         }
 
@@ -98,7 +100,37 @@ namespace TestInConsoleApp
             {
                 goDownDist += distance[goDownIndex];
             }
+
             return goDownIndex;
+        }
+
+
+        public int DistanceBetweenBusStops_Better(int[] distance, int start, int destination)
+        {
+            int totalDist = 0;
+            for (int i = 0; i < distance.Length; i++)
+            {
+                totalDist += distance[i];
+            }
+
+            int oneDirDist = 0;
+
+            //因为是一个环，所以按照正常顺序求和肯定是闭环的一个方向的总距离
+            //然后另外一个方向的总距离就等于环的总距离减去这一个方向的总距离
+            if (start > destination)
+            {
+                int temp = destination;
+                destination = start;
+                start = temp;
+            }
+
+            for (int i = start; i < destination; i++)
+            {
+                oneDirDist += distance[i];
+            }
+
+            int anotherDirDist = totalDist - oneDirDist;
+            return Math.Min(oneDirDist, anotherDirDist);
         }
     }
 }
