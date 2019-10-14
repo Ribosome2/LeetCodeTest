0556ac2f6d533f79167b679eb61662f7d1766ded
qinshilin <qinshilin@aobi.com>
2019-10-14 09:54:47 +0800

    二叉树的右视图

---
+++ TestInConsoleApp/TestInConsoleApp/CountBits_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp
+{
+    class CountBits_Test
+    {
+        public int[] CountBits(int num)
+        {
+            int[] ans = new int[num + 1];
+            for (int i = 1; i <= num; ++i)
+                ans[i] = ans[i & (i - 1)] + 1;
+            return ans;
+        }
+    }
+}
---
+++ TestInConsoleApp/TestInConsoleApp/Tree_RightSideView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp
+{
+    class Tree_RightSideView
+    {
+        public IList<int> RightSideView(TreeNode root)
+        {
+            List<int> resultList =new List<int>();
+            if (root != null)
+            {
+                Queue<TreeNode> queue = new Queue<TreeNode>();
+                queue.Enqueue(root);
+                while (queue.Count>0)
+                {
+                    int count = queue.Count;
+                    int number = 0;
+                    while (count>0)
+                    {
+                        var node = queue.Dequeue();
+                        number = node.val;
+                        if (node.left != null)
+                        {
+                            queue.Enqueue(node.left);
+                        }
+
+                        if (node.right != null)
+                        {
+                            queue.Enqueue(node.right);
+                        }
+
+                        count--;
+                    }
+                    resultList.Add(number);
+                }
+            }
+           
+          
+            return resultList;
+        }
+    }
+}
