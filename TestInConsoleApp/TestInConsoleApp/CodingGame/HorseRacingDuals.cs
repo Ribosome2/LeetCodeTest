ca0e208a9360652c0e414dd0da333f623299ad8c
qinshilin <qinshilin@aobi.com>
2019-12-24 15:58:18 +0800

    add coding game stuff

---
+++ TestInConsoleApp/TestInConsoleApp/CodingGame/HorseRacingDuals.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp.CodingGame
+{
+    class HorseRacingDuals
+    {
+        static void Main(string[] args)
+        {
+            int minDif = Int32.MaxValue;
+            int N = int.Parse(Console.ReadLine());
+            List<int> dataList = new List<int>();
+            for (int i = 0; i < N; i++)
+            {
+                int pi = int.Parse(Console.ReadLine());
+                dataList.Add(pi);
+            }
+
+            dataList.Sort();
+            for (int i = 1; i < dataList.Count; i++)
+            {
+                int dif = dataList[i] - dataList[i - 1];
+                if (dif < minDif)
+                {
+                    minDif = dif;
+                }
+            }
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine(minDif.ToString());
+        }
+    }
+}
---
+++ TestInConsoleApp/TestInConsoleApp/CodingGame/MIMITypeTest.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp.CodingGame
+{
+    class MIMITypeTest
+    {
+
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()); // Number of elements which make up the association table.
+            int Q = int.Parse(Console.ReadLine()); // Number Q of file names to be analyzed.
+            Dictionary<string, string> mDict = new Dictionary<string, string>();
+            for (int i = 0; i < N; i++)
+            {
+                string[] inputs = Console.ReadLine().Split(' ');
+                string EXT = inputs[0].ToLower(); // file extension
+                string MT = inputs[1]; // MIME type.
+                if (string.IsNullOrEmpty(EXT) == false)
+                {
+                    mDict[EXT] = MT;
+                }
+                Console.Error.WriteLine("kv : " + EXT + "  " + MT);
+            }
+            for (int i = 0; i < Q; i++)
+            {
+                string FNAME = Console.ReadLine(); // One file name per line.
+                Console.Error.WriteLine("fileName " + FNAME);
+                string[] inputs = FNAME.Split('.');
+                if (inputs.Length > 1)
+                {
+                    var key = inputs[inputs.Length-1].ToLower();
+                    Console.Error.WriteLine("key  " + key);
+                    if (mDict.ContainsKey(key))
+                    {
+                        Console.WriteLine(mDict[key]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("UNKNOWN");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("UNKNOWN");
+                }
+            }
+
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+            // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
+            Console.WriteLine("UNKNOWN");
+        }
+    }
+}
