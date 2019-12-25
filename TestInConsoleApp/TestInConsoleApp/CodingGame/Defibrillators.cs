ed82b7bdf1f6842f8c5631f757608a1d6c188281
qinshilin <qinshilin@aobi.com>
2019-12-25 10:52:06 +0800

    add coding game test

---
+++ TestInConsoleApp/TestInConsoleApp/CodingGame/Defibrillators.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp.CodingGame
+{
+    class Defibrillators
+    {
+        static void Test()
+        {
+            string LON = Console.ReadLine();
+            string LAT = Console.ReadLine();
+            int N = int.Parse(Console.ReadLine());
+            var longA =float.Parse(LON.Replace(',', '.'));
+            var latA = float.Parse(LAT.Replace(',', '.'));
+
+            double minDist = double.MaxValue;
+            string answer ="None";
+            for (int i = 0; i < N; i++)
+            {
+                string DEFIB = Console.ReadLine();
+                var arr = DEFIB.Split(';');
+                var defName = arr[1];
+                var longPos =float.Parse(arr[4].Replace(',', '.'));
+                var laPos = float.Parse(arr[5].Replace(',', '.'));
+                var dist = GetDist(longA, longPos, latA, laPos);
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    answer = defName;
+                }
+                Console.Error.WriteLine("Debug messages..." + DEFIB);
+            }
+
+            // Write an action using Console.WriteLine()
+            // To debug: Console.Error.WriteLine("Debug messages...");
+
+            Console.WriteLine(answer);
+        }
+
+        static double GetDist(float longA,float longB, float latA,float latB)
+        {
+            var x = (longB - longA) * Math.Cos(latA + latB);
+            var y = latB - latA;
+            return Math.Sqrt(x * x + y * y) * 6371;
+        }
+    }
+}
---
+++ TestInConsoleApp/TestInConsoleApp/CodingGame/SkynetRevolutionEpisode1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInConsoleApp.CodingGame
+{
+    class SkynetRevolutionEpisode1
+    {
+
+        public class SkyNetNode
+        {
+            public SkyNetNode(int index)
+            {
+                Id = index;
+            }
+            public int Id;
+            public List<SkyNetNode> Links =new List<SkyNetNode>();
+            public bool IsGateWay;
+        }
+
+        static void Main(string[] args)
+        {
+            string[] inputs;
+            inputs = Console.ReadLine().Split(' ');
+            int N = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+            int L = int.Parse(inputs[1]); // the number of links
+            int E = int.Parse(inputs[2]); // the number of exit gateways
+
+            SkyNetNode[] nodes = new SkyNetNode[N];
+            for (int i = 0; i < N; i++)
+            {
+                nodes[i]=new SkyNetNode();
+            }
+
+            for (int i = 0; i < L; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int N1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+                int N2 = int.Parse(inputs[1]);
+                nodes[N1].Links.Add(nodes[N2]);
+                nodes[N2].Links.Add(nodes[N1]);
+            }
+            for (int i = 0; i < E; i++)
+            {
+                int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
+                nodes[EI].IsGateWay = true;
+            }
+
+            // game loop
+            while (true)
+            {
+                int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
+
+
+                var agentLinks = nodes[SI].Links;
+                foreach (var node in agentLinks)
+                {
+                    
+                }
+                // Write an action using Console.WriteLine()
+                // To debug: Console.Error.WriteLine("Debug messages...");
+
+
+                // Example: 0 1 are the indices of the nodes you wish to sever the link between
+                Console.WriteLine("0 1");
+            }
+        }
+    }
+}
