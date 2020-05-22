fd3e772d1e22a9123c6647d036a7a9f332ef3158
qinshilin <qinshilin@aobi.com>
2020-05-22 15:25:54 +0800

    通过交换来判断重复的方法

--- TestInConsoleApp/剑指Offer/FindRepeatNumber_Test.cs
+++ TestInConsoleApp/剑指Offer/FindRepeatNumber_Test.cs
@@ -44,5 +44,45 @@ namespace 剑指Offer
 
             return -1;
         }
+
+        public int FindRepeatNumber2(int[] nums)
+        {
+//            算法流程
+//                遍历数组 nums ，设索引初始值为 i = 0:
+//            若 nums[i] == i ： 说明此数字已在对应索引位置，无需交换，因此执行 i += 1 与 continue ；
+//            若 nums[nums[i]] == nums[i] ： 说明索引 nums[i] 处的元素值也为 nums[i]，即找到一组相同值，返回此值 nums[i]；
+//            否则： 当前数字是第一次遇到，因此交换索引为 i 和 nums[i] 的元素值，将此数字交换至对应索引位置。
+//            若遍历完毕尚未返回，则返回 - 1−1 ，代表数组中无相同值。
+//
+//            作者：jyd
+//                链接：https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/solution/mian-shi-ti-03-shu-zu-zhong-zhong-fu-de-shu-zi-yua/
+//            来源：力扣（LeetCode）
+//            著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+           
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var number = nums[i];
+             
+
+                if (number == i)
+                {
+                    i++;
+                }
+                else
+                {
+                    var temp = nums[number];
+                    nums[number] = number;
+                    nums[i] = temp;
+                }
+                if (nums[number] == number)
+                {
+                    return number;
+                }
+
+
+            }
+
+            return -1;
+        }
     }
 }
