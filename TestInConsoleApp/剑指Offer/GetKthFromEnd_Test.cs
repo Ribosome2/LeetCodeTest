8723bb02275bb2e21e4a1c5204a2e889311597ac
qinshilin <qinshilin@aobi.com>
2020-05-28 17:33:01 +0800

    双指针寻找链表倒数第N个元素

---
+++ TestInConsoleApp/剑指Offer/GetKthFromEnd_Test.cs
@@ -0,0 +1,32 @@
+﻿namespace 剑指Offer
+{
+    public class GetKthFromEnd_Test
+    {
+        public ListNode GetKthFromEnd(ListNode head, int k)
+        {
+            if (head == null)
+            {
+                return null;
+            }
+
+            ListNode fastNode = head;
+            while (k>0)
+            {
+                if (fastNode == null)
+                {
+                    return null;
+                }
+
+                fastNode = fastNode.next;
+                k--;
+            }
+
+            while (fastNode!=null)
+            {
+                fastNode = fastNode.next;
+                head = head.next;
+            }
+            return head;
+        }
+    }
+}
