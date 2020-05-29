6edf1dbf05345f7380ac08ecd13b14819df4e2cb
qinshilin <qinshilin@aobi.com>
2020-05-29 14:49:10 +0800

    栈方式获得链表的反向输出

---
+++ TestInConsoleApp/剑指Offer/LinkListReversePrint_Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace 剑指Offer
+{
+    public class LinkListReversePrint_Test
+    {
+//        输入一个链表的头节点，从尾到头反过来返回每个节点的值
+        public int[] ReversePrint(ListNode head)
+        {
+            Stack<int> stack = new Stack<int>();
+            while (head!=null)
+            {
+                stack.Push(head.val);
+                head = head.next;
+            }
+            return stack.ToArray();
+        }
+    }
+}
