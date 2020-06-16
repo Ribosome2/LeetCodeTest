aed7a69d4b283679e2431490b6d444180f2e57fc
Ribosome2 <583154233@qq.com>
2020-06-16 21:03:18 +0800

    使用反射调用带out关键字的函数

---
+++ TestInConsoleApp/剑指Offer/InvokeMethodWith_OUT_KeyWord.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Reflection;
+
+namespace 剑指Offer
+{
+   
+        public class InvokeMethodWith_OUT_KeyWord
+        {
+            public InvokeMethodWith_OUT_KeyWord()
+            {
+                String str = "aa";
+                object[] parmsObj = new object[] { str };
+
+                var testObj = new TestClass();
+                Type actionType = testObj.GetType();
+                MethodInfo method = actionType.GetMethod("test", new Type[] { Type.GetType("System.String&") });
+                method.Invoke(testObj, parmsObj);
+                Console.WriteLine(parmsObj[0]);
+
+                object[] numberParmsObj = new object[] { 0 };
+                MethodInfo numberMethod= actionType.GetMethod("TestNumber", new Type[] { Type.GetType("System.Int32&") });
+                var result = numberMethod.Invoke(testObj, numberParmsObj);
+
+                Console.WriteLine("number "+numberParmsObj[0]+"  return value "+result);
+        }
+        }
+
+
+        public class TestClass
+        {
+
+            public int TestNumber(out int resultNumber)
+            {
+                resultNumber = 99;
+                return 33;
+            }
+        public void test(out string str)
+            {
+                str = "test1";
+            }
+
+            public void test(string str)
+            {
+                str = "test2";
+            }
+
+        }
+}
--- TestInConsoleApp/剑指Offer/Program.cs
+++ TestInConsoleApp/剑指Offer/Program.cs
@@ -10,14 +10,11 @@ namespace 剑指Offer
     {
         static void Main(string[] args)
         {
-            int[][] arr = new int[][]
-            {
-                new int[]{1,4,7,11,15},
-                new int[] {2,5,8,12,19},
-                new int[] {3,6,9,16,22},
-                new int[] {18,21,23,26,30},
-            };
-            Console.WriteLine(new MajorityElement_Test().MajorityElement1(new int[]{ 1, 2, 3, 2, 2, 2, 5, 4, 2 }));
+
+            Console.WriteLine("offer !!!!!!!!");
+            new InvokeMethodWith_OUT_KeyWord();
+
+
             Console.ReadKey();
         }
     }
