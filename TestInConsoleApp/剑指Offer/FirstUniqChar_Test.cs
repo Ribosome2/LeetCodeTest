7dec2c1f3d2767234e0415f5c74d17dcafae0d33
qinshilin <qinshilin@aobi.com>
2020-06-21 14:04:22 +0800

    查找第一个唯一的字符串

---
+++ TestInConsoleApp/剑指Offer/FirstUniqChar_Test.cs
@@ -0,0 +1,24 @@
+﻿namespace 剑指Offer
+{
+    public class FirstUniqChar_Test
+    {
+        public char FirstUniqChar(string s)
+        {
+            int[] arr= new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                var numIndex = s[i]-'a';
+                arr[numIndex]++;
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                var numIndex = s[i] - 'a';
+                if (arr[numIndex] == 1)
+                {
+                    return (char)('a' + numIndex);
+                }
+            }
+            return ' ';
+        }
+    }
+}
