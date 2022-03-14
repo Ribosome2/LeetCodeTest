5f16e2538a40645940a28b28b5d6d5285ce0c041
Ribosome2 <583154233@qq.com>
2022-03-14 08:21:14 +0800

    定义Dictionary最基础的数据结构，但是TValue和TKey到底是在哪里定义的？

---
+++ TestInConsoleApp/自己实现数据结构/Dictionary/KyleDictionary.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System;
+namespace System.Collections.Generic
+{
+    public class KyleDictionary
+    {
+
+        private struct Entry
+        {
+            public int hashCode;
+            public int next;
+//            public TValue value;
+        }
+
+        private Entry[] entries;
+        private int[] buckets;
+
+//        private void Insert(TKey)
+    }
+}
